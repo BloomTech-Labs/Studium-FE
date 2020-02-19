f15105275a8fa6a82b1d051cc9359e1f75c5b923
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-02-18 19:08:34 -0600

    Nothing changed.

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -33,6 +33,9 @@ export const signedIn = (user, dispatch) => {
 export const signout = (dispatch) => {
   debugger;
   localStorage.setItem('loggedIn', 'false');
+  // GIDSignIn.sharedInstance().signOut().then(() => {
+  //   dispatch(action(SIGNOUT))
+  // });
   firebase.auth().signOut().then(() => {
     dispatch(action(SIGNOUT))
   });
