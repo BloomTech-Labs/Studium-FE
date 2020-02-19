5cfae666b3814c2268cb6009c220db80d5f278a4
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-02-19 06:43:21 -0700

    Added styled title text for the dashboard and other view files.

---
+++ src/components/Styled/StyledTitleText.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTitleText = ( { text } ) => {
+  return ( <StyledTitle>
+    { text }
+  </StyledTitle> );
+};
+
+const StyledTitle = styled.div`
+color: #888888;
+font-weight: bold;
+font-size: 45px;
+margin-left: 6%;
+margin-top: 24px;
+text-align: left;
+margin-botom: 36px;
+`;
+
+export default StyledTitleText;
