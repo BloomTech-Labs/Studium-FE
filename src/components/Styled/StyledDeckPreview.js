594ff4df983dd12d277949d8b1eb672cd5959e11
Mada Gower <gmgower@gmail.com>
2020-02-20 22:29:20 -0600

    Add StyledAntdCard properites & propTypes.

--- src/components/Styled/StyledDeckPreview.js
+++ src/components/Styled/StyledDeckPreview.js
@@ -4,7 +4,41 @@ import PropTypes from 'prop-types';
 import {Card} from 'antd';
 
 const StyledDeckPreview = ({
+    text,
+    type = 'inner',
+    size = 'default',
+    icon,
+    loading,
+    block,
+    hoverable,
+    deck,
+    ...props
+}) => {
+    return (
+        <StyledAntdCard
+        type={type}
+        size={size}
+        icon={icon}
+        loading={loading && 'loading'}
+        block={block && 'block'}
+        {...props}        
+        >
+        {text}
+        </StyledAntdCard>
+    );
+};
 
-})
+StyledCardPreview.propTypes = {
+    text: PropTypes.string,
+    types: PropTypes.oneOf(['inner']),
+    size: PropTypes.oneOf(['default']),
+    icon: PropTypes.string,
+    loading: PropTypes.bool,
+    block: PropTypes.bool,
+    hoverable: PropTypes.bool,
+}
 
+const StyledDeckPreview = styled(Card)`
+
+`
 export default StyledDeckPreview;
