21617a92564c3be885b44efa82ab0f74f5ed23ab
Mada Gower <gmgower@gmail.com>
2020-02-20 22:14:39 -0600

    Initial commit.

---
+++ src/components/Styled/StyledDeckPreview.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import {Card} from 'antd';
+
+const StyledDeckPreview = ({
+
+})
+
+export default StyledDeckPreview;
