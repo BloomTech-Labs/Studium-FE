ce5a729db0199e33abc273e307b5be2275771155
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-02-23 16:20:09 -0600

    Updated .env to use staging instead of local Host and completed createAxios.

--- src/util/createAxios.js
+++ src/util/createAxios.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 
-
+//user Axios_React App
 export const userAxios = () => {
-    return axios.user()
+    return axios.create( {
         baseURL: process.env.REACT_APP_BASE_URL
+    }) ;  
+}
+
+export const connectAxiosAuth = (uid) => {
+    return axios.create({
+        baseURL: process.env.REACT_APP_BASE_URL, headers: {'Auth': uid},
+    })
 }
