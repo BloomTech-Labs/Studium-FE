25b5bfc09428a727fb237d6c7c070fe283781efb
Mada Gower <gmgower@gmail.com>
2020-02-23 14:38:21 -0600

    Refactor styles for preview deck.

--- src/views/PreviewDeck.js
+++ src/views/PreviewDeck.js
@@ -3,43 +3,52 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import StyledTitleText from '../components/Styled/StyledTitleText';
 import StyledDeckPreview from '../components/Styled/StyledDeckPreview';
+import StyledCreateCardBtn from '../components/Styled/StyledCreateCardBtn';
+import Footer from '../components/Footer/Footer';
 
 const decks = [
-  { card_name: 'Name this organ'}, 
-  { card_name: 'Define Proximal.'},
-  { card_name: '---- means towards the front.'}
-]
+  { card_name: 'Name this organ' },
+  { card_name: 'Define Proximal.' },
+  { card_name: '---- means towards the front' },
+];
 
 const PreviewDeck = props => {
   console.log('props', props);
-  return ( 
-  <StyledPreviewDeck>
-    <StyledTitleText text={ 'Preview' }/>
-    <StyledPreviewDeckHolder>
-      {decks.map(deck => {
-        return <StyledDeckPreview deck={deck} />
-      })}
-    <StyledDeckPreview  />
-    </StyledPreviewDeckHolder>
-  </StyledPreviewDeck> );
+  return (
+    <StyledPreviewDeck>
+      <StyledTitleText text={'Preview'} />
+      <StyledPreviewDeckHolder>
+        {decks.map(deck => {
+          console.log(decks)
+          return <StyledDeckPreview deck={deck} />;
+        })}
+        <StyledCreateCardBtn 
+    icon={'plus'}
+    size={'large'}
+    shape={'circle'}
+    >
+    </StyledCreateCardBtn>
+
+      </StyledPreviewDeckHolder>
+      <Footer/>
+    </StyledPreviewDeck>
+  );
 };
 
 PreviewDeck.propTypes = {};
 
 const StyledPreviewDeck = styled.div`
-display: flex;
-flex-direction: column;
-max-width: 100%;
-min-height: 90%;
-/* width: 100% */
+  display: flex;
+  flex-direction: column;
+  
+  max-width: 100%;
+  min-height: 90%;
+  width:100%;
 `;
 
 const StyledPreviewDeckHolder = styled.div`
-  max-width: 90%;
   display: flex;
-  flex-wrap: wrap;
-  margin:  0 auto;
-  justify-content: space-around;
-`
+  justify-content: space-evenly;
+`;
 
 export default PreviewDeck;
