278432b57b6b60da98aa145f2a91ebb0774d4fc6
austin breaux <47146701+austinbro5@users.noreply.github.com>
2020-02-23 09:28:20 -0800

    StyledCard componet view

--- src/components/Styled/StyledCard.js
+++ src/components/Styled/StyledCard.js
@@ -1,59 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import { Card, Icon} from 'antd';
 
 
-const StyledCard = ({
-  text = 'text',
-  type ,
-  size ,
-  icon ,
-  loading,
-  block,
-  hoverable,
-  ...props
-}) => {
-  return (
-    <StyledAntdCard
-      type={type}
-      size={size}
-      icon={icon}
-      loading={loading}
-      block={block}
-      {...props}
-    >
-    {icon && <Icon type={icon} />}
-    {text}
-    </StyledAntdCard>
-  );
-};
+const Card = ( {flashCard, flashImage} ) => { 
+    const [position, setPosition] = useState("front"); 
+    const flipCard = () => {
+        const newPos = position === "front" ? "back" : "front";
+        setPosition (newPos);
+    }
+    return (
+    <div style = {{position: "relative"}}>
+        <StyledCard position = {position} onClick = {flipCard} style = {{width:"285px", height:"421.56px"}}>
+            <CardText>
+                { position === "front" ? Card.question : Card.answer}
+           </CardText>
+        </StyledCard>
+        <StyledStackCard>
 
-StyledCard.propTypes = {
-  text: PropTypes.string, 
-  type: PropTypes.oneOf(['inner']),
-  size: PropTypes.oneOf(['default', 'small']),
-  icon: PropTypes.string,
-  loading: PropTypes.bool,
-  block: PropTypes.bool,
-  hoverable: PropTypes.bool,
-};
+        </StyledStackCard>
+    </div>
+    )
+}
 
-const StyledAntdCard = styled(Card)`
-  && {
-    width: 400px;
-    height: 240px;
-    left:  450px;
-    top: 90px;
-    padding: '0 auto';
-    border-radius: 45px;
-    background-color: ${props => {
-      if (props.type === 'primary') {
-        return props.theme.mainColor
-      } else {
-          return props.theme.white
-      }
-    }};
+const StyledStackCard = styled (Card)`
+&& {
+margin: 121px auto 0 auto; 
+background: "black";
+border-radius: 11px;
+position: absolute; 
+top: 5px; 
+width:"280px";
+height:"421.56px";
+z-index: -5px;
+    }
 `
 
-export default StyledCard;
+
+const StyledCard = styled (Card)`
+&& {
+margin: 121px auto 0 auto; 
+background: ${props => props.position === "front" ? "#F7F7F7" : "#1b1414c9" };
+color: ${props => props.position === "front" ? "#1b1414c9" : "white" };
+-webkit-box-shadow: 13px 18px 25px 0px rgba(33,32,33,1);
+-moz-box-shadow: 13px 18px 25px 0px rgba(33,32,33,1);
+box-shadow: 13px 18px 25px 0px rgba(33,32,33,1);
+border-radius: 11px;
+> .ant-card-body {
+    min-height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    }
+}
+transition: all 1s; 
+`
+const CardText = styled.div`
+height: 100%;
+display: flex; 
+justifyContent: center; 
+font-size: 20px; 
+font-weight: 900;
+transition: all 1s; 
+`
+export default Card; 
