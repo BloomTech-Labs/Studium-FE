eb5c8092d20cb857639b73979e97c18279d5f5ab
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2020-02-27 11:09:01 -0700

    Still trying to get github actions to see the tests.

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,19 @@ module.exports = {
   moduleDirectories: ['node_modules'], // ... other
   // options ...
   testEnvironment: 'node',
+  rootDir: __dirname + './src',
+  collectCoverageFrom: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!**/apidoc/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
