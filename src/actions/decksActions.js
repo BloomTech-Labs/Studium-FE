e7189fb8ed1e8986b6831d5e8ab9d736a6254dce
Mada Gower <gmgower@gmail.com>
2020-03-05 21:54:08 -0600

    action&reducer DELETE

--- src/actions/decksActions.js
+++ src/actions/decksActions.js
@@ -8,10 +8,10 @@ export const RECEIVE_DECKS_FAIL = 'RECEIVE_DECKS_FAIL';
 export const getDecks = (uid, dispatch) => {
   dispatch({type: GETTING_DECKS});
   createAxiosAuth(uid)
-    .get('/decks')
+    .get('/api/decks')
     .then(res => {
       debugger;
-    console.log("getDecks -> res", res)        
+      console.log('getDecks -> res', res);
       dispatch({type: RECEIVE_DECKS, payload: res.data});
     })
     .catch(error => {
@@ -24,22 +24,42 @@ export const getDecks = (uid, dispatch) => {
 };
 
 // Creates a new deck
-export const POSTING_DECKS_START = 'POSTING_DECKS_START';
-export const POSTING_DECKS_SUCCESS = 'POSTING_DECKS_SUCCESS';
-export const POSTING_DECKS_FAILURE = 'POSTING_DECKS_SUCCESS';
+export const POSTING_DECK_START = 'POSTING_DECK_START';
+export const POSTING_DECK_SUCCESS = 'POSTING_DECK_SUCCESS';
+export const POSTING_DECK_FAILURE = 'POSTING_DECK_FAILURE';
 
-export const createDecks = (uid, dispatch) => {
-    dispatch({type: POSTING_DECKS_START});
-    createAxiosAuth(uid)
-    .get('/dicks')
+export const postDeck = (uid, dispatch) => {
+  dispatch({type: POSTING_DECK_START});
+  createAxiosAuth(uid)
+    .get('/api/decks')
     .then(res => {
-        dispatch({type: POSTING_DECKS_SUCCESS, payload: res.data})
+      dispatch({type: POSTING_DECK_SUCCESS, payload: res.data});
     })
-    .catch(err => {
+    .catch(error => {
       dispatch({
-        type: POSTING_DECKS_FAILURE,
-        error: ''
-      })
+        type: POSTING_DECK_FAILURE,
+        error: 'Failed to post your deck',
+      });
+    });
+};
 
+//Deletes a single deck
+export const DELETING_DECK_START = 'DELETING_DECK_START';
+export const DELETING_DECK_SUCCESS = 'DELETING_DECK_SUCCESS';
+export const DELETING_DECK_FAILURE = 'DELETING_DECK_FAILURE';
+
+export const deleteDeck = (uid, dispatch) => {
+  dispatch({type: DELETING_DECK_START});
+  createAxiosAuth(uid)
+  .get('/api/dicks/:id')
+  .then(res => {
+    dispatch({type: DELETING_DECK_SUCCESS, payload: res.data});
+  })
+  .catch(error => {
+    dispatch({
+      type: DELETING_DECK_FAILURE, 
+      error: 'Failed to delete you deck.'
     })
+  })
+  
 }
--- src/reducers/decksReducer.js
+++ src/reducers/decksReducer.js
@@ -2,8 +2,12 @@ import {
   GETTING_DECKS,
   RECEIVE_DECKS,
   RECEIVE_DECKS_FAIL,
-  POSTING_DECKS_START,
-  POSTING_DECKS_SUCCESS 
+  POSTING_DECK_START,
+  POSTING_DECK_SUCCESS,
+  POSTING_DECK_FAILURE,
+  DELETING_DECK_START,
+  DELETING_DECK_SUCCESS,
+  DELETING_DECK_FAILURE,
 } from '../actions';
 
 const initialState = {
@@ -32,10 +36,40 @@ export const deckReducer = (state = initialState, action) => {
       };
 
     // Creates a new deck
-    case POSTING_DECKS_START:
+    case POSTING_DECK_START:
       return {...state, fetchingDecks: true, errorDecksMessage: undefined};
-    case POSTING_DECKS_SUCCESS:
-    return {...state, iFetching: false, decks: action.payload, errorDecksMessage: undefined};
+    case POSTING_DECK_SUCCESS:
+      return {
+        ...state,
+        fetchingDecks: false,
+        decks: action.payload,
+        errorDecksMessage: undefined,
+      };
+    case POSTING_DECK_FAILURE:
+      return {
+        ...state,
+        fetchingDecks: false,
+        decks: [],
+        errorDecksMessage: action.error,
+      };
+
+    // Deletes a single deck
+    case DELETING_DECK_START:
+      return {...state, fetchingDecks: true, errorDecksMessage: undefined};
+    case DELETING_DECK_SUCCESS:
+      return {
+        state,
+        fetchingDecks: false,
+        decks: action.payload,
+        errorDecksMessage: undefined,
+      };
+    case DELETING_DECK_FAILURE:
+      return {
+        ...state,
+        fetchingDecks: false,
+        decks: [],
+        errorDecksMessage: action.error,
+      };
     default:
       return state;
   }
