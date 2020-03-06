13e78a72494b1c2522ca9db40b09e24e6d7d50ba
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-03-05 21:35:34 -0600

    Created card types.

--- src/actions/cardActions.js
+++ src/actions/cardActions.js
@@ -1,1 +1,8 @@
 import {action} from './action';
+import {createAxiosAuth} from '../utilities/createAxios';
+
+export const CREATE_CARD = 'CREATE_CARD';
+export const UPDATE_CARD = 'UPDATE_CARD';
+export const DELETE_CARD = 'DELETE_CARD';
+
+//Do we need to do a fail card?
