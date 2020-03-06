9807e7a4149751d5493f16b50724508f5af953c2
Chris Cruz <christopherc1331@gmail.com>
2020-03-05 20:14:52 -0800

    First commit

---
+++ src/components/Styled/StyledCreateCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const CreateCard = props => {
+  return <StyledCreateCard></StyledCreateCard>;
+};
+
+CreateCard.propTypes = {};
+
+const StyledCreateCard = styled.div`
+  width: 100%;
+`;
+
+export default CreateCard;
