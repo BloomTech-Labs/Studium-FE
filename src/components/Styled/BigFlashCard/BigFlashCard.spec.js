64894927b0ef506e00f859b364ecef16534385a0
unknown <ccurry20@gmail.com>
2020-03-08 14:22:41 -0400

    Big Flash Card Test

--- src/components/Styled/BigFlashCard/BigFlashCard.spec.js
+++ src/components/Styled/BigFlashCard/BigFlashCard.spec.js
@@ -11,15 +11,29 @@ import {
   toHaveTextContent,wait
 } from '../../../utilities/test-utils';
 import BigFlashCard from './BigFlashCard';
-import {renderer, act, create} from 'react-test-renderer';
+import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
+
 
 
 /**
  * Test the big flash card
  */
 
+// test('BigFlashCard render correctly', () => {
+//   const tree = renderer.create(<BigFlashCard flashCard={{question: "This is" +
+//       " a question", answer: "This is the answer."}} />).toJSON();
+//   expect(tree).toMatchSnapshot();
+// });
+
 describe('Big Flash Card', () => {
+
+  test('BigFlashCard render correctly', () => {
+    const tree = renderer.create(<BigFlashCard flashCard={{question: "This is" +
+        " a question"}} />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
     test('position front', () => {
     const { container, getByText } = render(<BigFlashCard flashCard = {{question: "question", answer: "answer"}}/>)
     expect(getByText('question')).toBeInTheDocument()
@@ -31,4 +45,6 @@ describe('Big Flash Card', () => {
     expect(queryByText('answer')).toBeInTheDocument()
     //debug(queryByText('answer'))
   })
+
+ 
 });
