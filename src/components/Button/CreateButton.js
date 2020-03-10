a8701e1a571a9c336ec639969d582744c96e113a
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-03-09 22:34:30 -0500

    Updated File name to debug correctly

--- src/components/Button/CreateButton.js
+++ src/components/Button/CreateButton.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
 
-export const cardaddButton = ({
+export const CreateButton = ({
     text, type = 'primary', size = 'default', icon, shape, loading, block, color, ...props
 }) => {
     
