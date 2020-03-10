ec79f015ec9f5a514b67c41b0fdc25894d6d85a7
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-03-10 12:54:17 -0500

    DEBUGGED BUTTON. AND IT LOOKS GORGEOUS.

--- src/components/Button/CreateButton.js
+++ src/components/Button/CreateButton.js
@@ -2,26 +2,36 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
-import plus from '../../images/plus.png';
+import {ReactComponent as Svg } from '../../images/Union.svg';
 
 export const CreateButton = ({
     text, type = 'primary', size = 'default', icon, shape, loading, block, color, ...props
 }) => {
     return (
         <Styled > 
-            <img src = {plus} />
+            <Svg /> 
         </Styled>
     )
 }
 
 const Styled = styled(Button)`
 &&{
-    background-image: url(${plus});
+    display: flex;
+    box-sizing: border-box;
+    position: relative;
     background: #4CB69F;
     border-radius: 100%;
-    width: 20px;
-    > img {
-        height: "100%";
-        width: "100%";
+    height: 69px;
+    width: 69px;
+    border: 7px solid white;
+    justify-content: center;
+    align-items: center;
+    > Svg {
+        height:33px;
+        width: 33px;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%); 
+        position: absolute;
     }
 }`;
