b76b77b377bca7a97db37342efc4d41d5a2d1e21
Chris Cruz <christopherc1331@gmail.com>
2020-03-11 21:54:19 -0700

    Added action.payload.value check

--- src/reducers/photoReducer.js
+++ src/reducers/photoReducer.js
@@ -1,5 +1,7 @@
 import {
-  UPLOADING_PHOTO_FAILED, UPLOADING_PHOTO_SUCCESS, UPLOADING_PHOTO_PROGRESS,
+  UPLOADING_PHOTO_FAILED,
+  UPLOADING_PHOTO_SUCCESS,
+  UPLOADING_PHOTO_PROGRESS,
   UPLOADING_PHOTO_INIT,
 } from '../actions/photo.js';
 
@@ -31,11 +33,16 @@ const initialState = {
  * @param {Action} action
  * @returns {PhotoReducerState} state
  */
-export const photosReducer = ( state = initialState, action ) => {
-  switch( action.type ){
+export const photosReducer = (state = initialState, action) => {
+  debugger;
+  switch (action.type) {
     case 'SET_INIT_STATE':
-      if( action.payload && action.payload.name &&
-        action.payload.name.includes( 'photos' ) ){
+      if (
+        action.payload &&
+        action.payload.name &&
+        action.payload.name.includes('photos') &&
+        action.payload.value
+      ) {
         return action.payload.value;
       }
       return state;
@@ -43,9 +50,9 @@ export const photosReducer = ( state = initialState, action ) => {
     case UPLOADING_PHOTO_FAILED:
     case UPLOADING_PHOTO_SUCCESS:
     case UPLOADING_PHOTO_PROGRESS:
-      state.photos[ action.payload.id ] = action.payload;
-      return { photos: { ...state.photos } };
-    
+      state.photos[action.payload.id] = action.payload;
+      return {photos: {...state.photos}};
+
     default:
       return state;
   }
