787a43353d9722f7ae75285f258831705317b21c
Chris Cruz <christopherc1331@gmail.com>
2020-03-17 22:17:59 -0700

    Changed logger settings

--- src/utilities/debugLogger.js
+++ src/utilities/debugLogger.js
@@ -1,7 +1,7 @@
-import chalk from "chalk";
-import moment from "moment";
-import { SYNAPS_CONFIG } from "../synapsConfig.js";
-import { LOG_TYPES } from "./constants.js";
+import chalk from 'chalk';
+import moment from 'moment';
+import {SYNAPS_CONFIG} from '../synapsConfig.js';
+import {LOG_TYPES} from './constants.js';
 
 const log = console.log;
 const debugTextSize = SYNAPS_CONFIG.debugLogTextSize;
@@ -15,44 +15,55 @@ const debugTextSize = SYNAPS_CONFIG.debugLogTextSize;
  */
 
 let currentGroup = null;
-export const logOutMessageOrDebug = ( message, debug ) => {
+export const logOutMessageOrDebug = (message, debug) => {
   // if we were unable to load the config in the beginnig then we don't
   // handle any logging.
-  if( SYNAPS_CONFIG.debugError && message.logType !== LOG_TYPES.ERROR ){
+  if (SYNAPS_CONFIG.debugError && message.logType !== LOG_TYPES.ERROR) {
     return;
   }
-  
-  if( process.env.REACT_APP_SHOW_LOGS || message.logType === LOG_TYPES.ERROR ){
-    
-    if( !message.location ){
+
+  if (process.env.REACT_APP_SHOW_LOGS || message.logType === LOG_TYPES.ERROR) {
+    if (!message.location) {
       return logToConsoleTest(
-        createMessage( "You must provide debug logger with a" +
-          " app location so debug logger can conditionally log info messages" +
-          " from only the parts of the app that are being debugged. You will" +
-          " also need to add your app location to the" +
-          " REACT_APP_DEBUG_LOCATIONS array in the env variables.",
+        createMessage(
+          'You must provide debug logger with a' +
+            ' app location so debug logger can conditionally log info messages' +
+            ' from only the parts of the app that are being debugged. You will' +
+            ' also need to add your app location to the' +
+            ' REACT_APP_DEBUG_LOCATIONS array in the env variables.',
           LOG_TYPES.WARNING,
-          "Debug Logger",
-        ) );
+          'Debug Logger'
+        )
+      );
     }
-    groupConsole( message, debug );
+    groupConsole(message, debug);
   }
 };
 
-const groupConsole = ( message, debug ) => {
-  if( SYNAPS_CONFIG.appsToDebbug.includes( message.location ) ||
-    message.location === "Debug Logger" || message.location === "App" ){
-    let colotStr = "background: #0C2545; color: white; padding: 5px 2px;";
-    if( currentGroup === null ){
+const groupConsole = (message, debug) => {
+  if (
+    SYNAPS_CONFIG.appsToDebbug.includes(message.location) ||
+    message.location === 'Debug Logger' ||
+    message.location === 'App'
+  ) {
+    let colotStr = 'background: #0C2545; color: white; padding: 5px 2px;';
+    if (currentGroup === null) {
       currentGroup = message.location;
-      console.groupCollapsed( `%c ${ currentGroup }`, colotStr );
-    }else if( currentGroup !== message.location ){
-      console.groupEnd( currentGroup );
+      if (SYNAPS_CONFIG.groupLogsTogether) {
+        console.groupCollapsed(`%c ${currentGroup}`, colotStr);
+      } else {
+        console.group(`%c ${currentGroup}`, colotStr);
+      }
+    } else if (currentGroup !== message.location) {
+      console.groupEnd(currentGroup);
       currentGroup = message.location;
-      console.groupCollapsed( `%c ${ currentGroup }`, colotStr );
-      
+      if (SYNAPS_CONFIG.groupLogsTogether) {
+        console.groupCollapsed(`%c ${currentGroup}`, colotStr);
+      } else {
+        console.group(`%c ${currentGroup}`, colotStr);
+      }
     }
-    checkForAndLogOutObject( message, debug );
+    checkForAndLogOutObject(message, debug);
   }
 };
 
@@ -61,49 +72,46 @@ const groupConsole = ( message, debug ) => {
  * @param message
  * @param debug
  */
-const checkForAndLogOutObject = ( message, debug ) => {
-  
-  if( message.logType === LOG_TYPES.OBJECT || message.logType === undefined ){
-    console.groupCollapsed( "Object from " + message.location );
-    if( message.logType === undefined ){
-      console.dir( message );
-    }else{
-      console.dir( message.message );
+const checkForAndLogOutObject = (message, debug) => {
+  if (message.logType === LOG_TYPES.OBJECT || message.logType === undefined) {
+    console.groupCollapsed('Object from ' + message.location);
+    if (message.logType === undefined) {
+      console.dir(message);
+    } else {
+      console.dir(message.message);
     }
     console.groupEnd();
     return;
   }
-  
-  if( process.env.NODE_ENV === "test" ){
-    logToConsoleTest( message, debug );
-  }else{
-    logToBrowserTerminal( message );
+
+  if (process.env.NODE_ENV === 'test') {
+    logToConsoleTest(message, debug);
+  } else {
+    logToBrowserTerminal(message);
   }
-  
 };
 
 /**
  * Log message to the browser terminal
  * @param message
  */
-export const logToBrowserTerminal = ( message ) => {
-  if( message !== undefined ){
-    let str = "";
-    str += `${ message.message }  --->  ${ moment().format( "LLL" ) }`;
-    let colotStr = "";
-    if( message.logType === LOG_TYPES.INFO ){
-      colotStr = "background: #7AD7F0; color: black; padding: 5px 2px;";
-    }else if( message.logType === LOG_TYPES.ERROR ){
-      colotStr = "background: red;";
-    }else if( message.logType === LOG_TYPES.WARNING ){
-      colotStr = "background: yellow;";
+export const logToBrowserTerminal = message => {
+  if (message !== undefined) {
+    let str = '';
+    str += `${message.message}  --->  ${moment().format('LLL')}`;
+    let colotStr = '';
+    if (message.logType === LOG_TYPES.INFO) {
+      colotStr = 'background: #7AD7F0; color: black; padding: 5px 2px;';
+    } else if (message.logType === LOG_TYPES.ERROR) {
+      colotStr = 'background: red;';
+    } else if (message.logType === LOG_TYPES.WARNING) {
+      colotStr = 'background: yellow;';
     }
-    colotStr += ` font-size: ${ debugTextSize }`;
-    log( `%c${ str }`, colotStr );
-    console.groupCollapsed( "Debug" );
-    console.trace( "Stack trace" );
-    console.groupEnd( "Debug" );
-    
+    colotStr += ` font-size: ${debugTextSize}`;
+    log(`%c${str}`, colotStr);
+    console.groupCollapsed('Debug');
+    console.trace('Stack trace');
+    console.groupEnd('Debug');
   }
 };
 
@@ -112,33 +120,29 @@ export const logToBrowserTerminal = ( message ) => {
  * @param message
  * @param debug
  */
-const logToConsoleTest = ( message, debug ) => {
-    if( debug !== false && debug !== undefined ){
-      debug();
+const logToConsoleTest = (message, debug) => {
+  if (debug !== false && debug !== undefined) {
+    debug();
+  }
+  if (message !== undefined) {
+    let str = '';
+    if (message.location) {
+      str = `${message.location}: `;
     }
-    if( message !== undefined ){
-      
-      let str = "";
-      if( message.location ){
-        str = `${ message.location }: `;
-      }
-      str += `${ message.message }`;
-      
-      if( message.logType === "error" ){
-        
-        log( chalk.bgRed.bold( str ) );
-      }else if( message.logType === "info" ){
-        let blue = chalk.bgBlue( str );
-        log( chalk.bgBlue( str ) );
-      }else if( message.logType === "warning" ){
-        log( chalk.bgYellow( str ) );
-      }else{
-        log( str );
-      }
+    str += `${message.message}`;
+
+    if (message.logType === 'error') {
+      log(chalk.bgRed.bold(str));
+    } else if (message.logType === 'info') {
+      let blue = chalk.bgBlue(str);
+      log(chalk.bgBlue(str));
+    } else if (message.logType === 'warning') {
+      log(chalk.bgYellow(str));
+    } else {
+      log(str);
     }
   }
-;
-
+};
 /**
  *
  * @param {string} message
@@ -146,18 +150,24 @@ const logToConsoleTest = ( message, debug ) => {
  * @param {string} location
  * @return {DebugLogMessage}
  */
-export const createMessage = ( message, logType = LOG_TYPES.ERROR,
-  location = "unknown" ) => {
-  if( location === "unknown" ){
+export const createMessage = (
+  message,
+  logType = LOG_TYPES.ERROR,
+  location = 'unknown'
+) => {
+  if (location === 'unknown') {
     logOutMessageOrDebug(
-      createMessage( "You must provider a location if you" +
-        " are trying to use the debug logger. Pass in message, logType, and" +
-        " location into the create message function.", LOG_TYPES.WARNING,
-        "Debug Logger",
-      ) );
+      createMessage(
+        'You must provider a location if you' +
+          ' are trying to use the debug logger. Pass in message, logType, and' +
+          ' location into the create message function.',
+        LOG_TYPES.WARNING,
+        'Debug Logger'
+      )
+    );
   }
-  
-  return { message, logType, location };
+
+  return {message, logType, location};
 };
 
 /**
