43b43a08caf23d63a18239fe90aca04dcb721993
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-07 20:49:40 -0500

    Updating file names to match things. Testing works but still has bugs.

--- src/views/Testing.js
+++ src/views/Testing.js
@@ -1,12 +1,15 @@
-import React from "react";
-import styled from "styled-components";
-import SecondAnimatedSynapsComponent
-  from "../svgComponents/SecondAnimatedSynapsComponent.js";
+import React from 'react';
+import styled from 'styled-components';
+import { SynapsBrain } from '../components/SynapsBrain/SynapsBrain.js';
+import { CreateButton } from '../components/Button/CreateButton.js';
+import SvgComponent from '../images/svgAnimation/CardAnimation.js';
+import { SmallFlashCard } from '../components/SmallFlashCard/SmallFlashCard.js';
+import {PreviewDeckCards} from '../components/PreviewDeckCards/PreviewDeckCards.js';
 
-export const Testing = (props) => {
-  return (<StyledTesting>
-    <SecondAnimatedSynapsComponent width={"1000px"} height={"1000px"}/>
-  </StyledTesting>);
+export const Testing = () => {
+  return ( <StyledTesting>
+    <SvgComponent />
+  </StyledTesting> );
   
 };
 
