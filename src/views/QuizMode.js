db22cd1c4f3a7a3fd3fec039bdb525d88bde5f9a
Brimes7 <48000565+Brimes7@users.noreply.github.com>
2020-04-09 22:58:09 -0500

    Completed the 2 buttons

--- src/views/QuizMode.js
+++ src/views/QuizMode.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import {TitleText} from '../components/Text/TitleText.js'
 import {useAppHooks} from './customHooks/useAppHooks.js';
 import {BigFlashCard} from '../components/BigFlashCard.js';
+import {ReactComponent as SvgBack} from  '../svgs/BackButton.svg';
+import {ReactComponent as SvgNext} from '../svgs/NextButton.svg';
 
 export default function QuizMode({deck}) {
     const {cardsState} = useAppHooks("QuizMode")
@@ -21,12 +23,24 @@ export default function QuizMode({deck}) {
         })
         setNotViewed([...newArray, ...notViewed])
     })
+
+    function back({}) {
+            let lastCard = Viewed.pop();
+            setNotViewed([...notViewed, lastCard])
+            setViewed([...Viewed, lastCard])
+    }
+    function next({}) {
+            let firstCard = notViewed.shift();
+            setNotViewed([...notViewed, firstCard])
+            setViewed([...Viewed, firstCard])
+    }
+
     return (
         <Quizmode>
-            <TitleText text = {deck.deck_name} color = {"#2A685B"}>
+            <TitleText text = {deck.deck_name} color = {"#2A685B"}/>>
             <BigFlashCard flashCard = {notViewed[0]}> </BigFlashCard>
-
-            </TitleText>
+            <SvgBack onClick = {back}/>
+            <SvgNext onClick = {next}/>
         </Quizmode>
     )
 }
@@ -35,4 +49,7 @@ const Quizmode = styled.div `
     width = 100%;
     height = 100%;
     background-color: white;
+`
+const SvgBack = styled.div `
+
 `
