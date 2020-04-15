37b73646ac9e47f996dcd58d310450e584571851
Mada Gower <gmgower@gmail.com>
2020-04-14 22:33:04 -0500

    Add Prop-types.

--- src/components/Modal/modal.js
+++ src/components/Modal/modal.js
@@ -1,5 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import {SynapsButton} from '../components';
+import {Button, Modal} from 'antd';
 
-export const Modal = ({
+
+export const SynapsModal = ({
+    centered,
+    onOk,
+    onCancel,
+    zIndex,
+    mask,
+    ...props
+}) => {
+    return (
+        <StyledAntdModal
+        centered={centered}
+        onOk={onOk}
+        onCancel={onCancel}
+        zIndex={zIndex}
+        mask={mask}
+        >
+
+        </StyledAntdModal>
+    )
+}
+
+SynapsModal.propTypes = {
+    centered: PropTypes.bool,
     
-})
+}
+
+const StyledAntdModal = styled(Modal)`
+
+`
