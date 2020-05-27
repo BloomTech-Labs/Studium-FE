3a86c0fbb8e06d9374680225d9f5a27d4667ddf0
Anthony Lopez <53539912+AnthonyLopez1120@users.noreply.github.com>
2020-05-26 20:31:56 -0500

    Delete CreateButton.test.js

--- src/components/Button/CreateButton.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-
-import {CreateButton} from './CreateButton.js';
-
-test('Create Button works', () => {
-    const tree = renderer.create(<CreateButton />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
