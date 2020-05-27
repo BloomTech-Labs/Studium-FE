593251e5b9e693c0e4f3b682060a87eb16653a04
Anthony Lopez <53539912+AnthonyLopez1120@users.noreply.github.com>
2020-05-26 20:30:47 -0500

    Delete Avatar.test.js

--- src/components/Avatar/Avatar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {customRender} from '../../utilities/test-utils.js';
-
-import {NavBarAvatar} from './NavBarAvatar.js';
-
-test('StyledAvatar render correctly', () => {
-  const {container} = customRender(<NavBarAvatar/>);
-  expect(container).toMatchSnapshot();
-});
