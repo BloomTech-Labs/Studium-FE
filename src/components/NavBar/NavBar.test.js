c0f82bc4e02d336c62da2579d5419183ebc80d28
Anthony Lopez <53539912+AnthonyLopez1120@users.noreply.github.com>
2020-05-26 20:34:54 -0500

    Delete NavBar.test.js

--- src/components/NavBar/NavBar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {customRender} from '../../utilities/test-utils.js';
-
-import {NavBar} from './NavBar.js';
-
-test('StyledNavBar render correctly', () => {
-  const {container} = customRender(<NavBar />);
-  expect(container).toMatchSnapshot()
-});
