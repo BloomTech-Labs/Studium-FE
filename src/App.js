c370b04f4cc54357581d198aee024fd1c1daaca5
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-06-16 22:37:32 -0400

    okta auth research

--- src/App.js
+++ src/App.js
@@ -19,6 +19,15 @@ function App() {
     setUserList(users)
   }
 
+  // function onSignIn(googleUser) {
+  //   var profile = googleUser.getBasicProfile();
+  //   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  //   console.log('Name: ' + profile.getName());
+  //   console.log('Image URL: ' + profile.getImageUrl());
+  //   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  // }
+  
+
   return (
     <div className="App">
       <h2>Studium Front-End</h2>
@@ -37,7 +46,8 @@ function App() {
 
             : null
         }
-      
+      {/* <div class="g-signin2" data-onsuccess="onSignIn"></div>
+      <button>Console Log User Details</button> */}
     </div>
   );
 }
