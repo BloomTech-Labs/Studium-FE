4358d7e758215a3c836e7868bb4e2c647fe77e2e
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-06-17 22:11:04 -0400

    app.js cleanup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Route } from 'react-router-dom';
 import SplashPage from './components/splash page/SplashPage';
 import Register from './components/register/Register'
