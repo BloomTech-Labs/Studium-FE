8182588179cf62136942219dc79ea952c091cbd3
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-06-20 13:16:00 -0400

    oktafix

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,24 +1,16 @@
-// const OKTA_DOMAIN = process.env.DOMAIN;
-// const CLIENT_ID = process.env.CLIENT_ID;
-// const CALLBACK_PATH = '/implicit/callback';
+const OKTA_DOMAIN = process.env.DOMAIN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CALLBACK_PATH = '/implicit/callback';
 
-// const ISSUER = `https://${OKTA_DOMAIN}/oauth2/default`;
-// const HOST = window.location.host;
-// const REDIRECT_URI = `http://${HOST}${CALLBACK_PATH}`;
-// const SCOPES = 'openid profile email';
-
-// export const config = {
-//    issuer: ISSUER,
-//    clientId: '0oaekug1e6udJlXUA4x6',
-//    redirectUri: REDIRECT_URI,
-//    scope: SCOPES.split(/\s+/),
-// };
+const ISSUER = `https://${OKTA_DOMAIN}/oauth2/default`;
+const HOST = window.location.host;
+const REDIRECT_URI = `http://${HOST}${CALLBACK_PATH}`;
+const SCOPES = 'openid profile email';
 
 export const config = {
+   issuer: ISSUER,
    clientId: '0oaekug1e6udJlXUA4x6',
-   issuer: 'https://dev-360882.okta.com/oauth2/default',
-   redirectUri: 'http://localhost:3000/implicit/callback',
-   scopes: ['openid', 'profile', 'email'],
-   pkce: true
- };
+   redirectUri: REDIRECT_URI,
+   scope: SCOPES.split(/\s+/),
+};
  
