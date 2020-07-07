c2a1d5af4fba88b78746d5bff8b6e8566cc6523b
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-07-06 22:36:08 -0400

    added the <a> and styled it so there are no underlines on the google button

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { LoginScreen, TextBox, TextBox2, Button, Nav, H3, GoogButton, H4, HRline, HRline2 } from "./loginstyles"
+import { LoginScreen, TextBox, TextBox2, Button, Nav, H3, GoogButton, H4, HRline, HRline2, AnchorButton } from "./loginstyles"
 import AxiosWithAuth from '../../utils/axiosWithAuth.js'
 import GoogleButton from 'react-google-button'
 
@@ -36,10 +36,14 @@ const Login = props => {
                 <H3>Studium</H3>
             </Nav>
             <GoogButton>
+                <AnchorButton href='https://dev-360882.okta.com/oauth2/v1/authorize?idp=0oael0y3cwL5zx7Tn4x6&client_id=0oaekug1e6udJlXUA4x6&response_type=id_token&response_mode=fragment&scope=openid profile email&redirect_uri=https://studium-app.net/dashboard&state=4Oycg4JA0a
+&nonce=55QzavEtgu'>
                 <GoogleButton
                     type="dark"
                     onClick={() => { console.log('This button can be clicked') }}
-                /></GoogButton>
+                />
+                </AnchorButton> {/* This <a> is being used to help redirect our google button to okta*/}
+                </GoogButton>
             <HRline></HRline>
             <H4>Or Email</H4>
             <HRline2></HRline2>
