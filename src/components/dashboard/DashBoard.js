2d92fece27ebb8fde0f99449f914d5c5752fbf7d
Nicole Nolla <22096240+sicklesium@users.noreply.github.com>
2020-07-06 20:59:24 -0400

    Dashboard pretty much built out and completed, need to add functionality but the base is there

--- src/components/dashboard/DashBoard.js
+++ src/components/dashboard/DashBoard.js
@@ -1,31 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import { useOktaAuth } from '@okta/okta-react'
+import { Deck, Labels, Decks, Title, DeckTitle, CardCount, CreateDeck, LabelsTwo } from './dashboardStyles.js'
+import { H1, NavBar } from '../navigation/NavBarDash.js'
 
 const DashBoard = () => {
-   const { authState, authService } = useOktaAuth();
-   const [userInfo, setUserInfo] = useState(null);
+  const { authState, authService } = useOktaAuth();
+  const [userInfo, setUserInfo] = useState(null);
 
-   useEffect(() => {
-      if (!authState.isAuthenticated) {
-        // When user isn't authenticated, forget any user info
-        setUserInfo(null);
-      } else {
-        authService.getUser().then((info) => {
-          setUserInfo(info);
-        });
-      }
-    }, [authState, authService]); // Update if authState changes
+  useEffect(() => {
+    if (!authState.isAuthenticated) {
+      // When user isn't authenticated, forget any user info
+      setUserInfo(null);
+    } else {
+      authService.getUser().then((info) => {
+        setUserInfo(info);
+      });
+    }
+  }, [authState, authService]); // Update if authState changes
 
-   return (
-      <div>
-         DashBoard
-         { userInfo && 
-           <div>
-             <p>Welcome back, {userInfo.name}!</p>
-           </div>
-         }
-      </div>
-   )
+  return (
+    <div>
+      <NavBar>
+        <H1>Studium</H1>
+      </NavBar>
+      <Title>Card Collection</Title>
+      <Decks>
+        <Deck>
+          <Labels>
+            <DeckTitle>Deck Title</DeckTitle>
+            <CardCount>2 cards</CardCount>
+          </Labels>
+        </Deck>
+        <Deck>
+          <LabelsTwo>
+            <CreateDeck>Create a Deck</CreateDeck>
+          </LabelsTwo>
+        </Deck>
+      </Decks>
+    </div>
+  )
 }
 
 export default DashBoard
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,67 +1,67 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
-import {LoginScreen, TextBox, TextBox2, Button, Nav, H3, GoogButton, H4, HRline, HRline2} from "./loginstyles"
-import { axiosWithAuth } from '../../utils/axiosWithAuth';
+import { LoginScreen, TextBox, TextBox2, Button, Nav, H3, GoogButton, H4, HRline, HRline2 } from "./loginstyles"
+import AxiosWithAuth from '../../utils/axiosWithAuth.js'
 import GoogleButton from 'react-google-button'
 
 
 const Login = props => {
     const [login, setLogin] = useState({
         username: '',
-        password:''
+        password: ''
     })
 
-const handleChange = e => {
-    setLogin({
-        ...login,
-        [e.target.name]: e.target.value
-    });
-};
+    const handleChange = e => {
+        setLogin({
+            ...login,
+            [e.target.name]: e.target.value
+        });
+    };
 
-const handleSubmit = e => {
-    e.preventDefault();
-    AxiosWithAuth()
-       .post('/auth/login', login) 
-    .then(
-        res =>{
-            localStorage.setItem('token', res.data.token);
+    const handleSubmit = e => {
+        e.preventDefault();
+        AxiosWithAuth()
+            .post('/auth/login', login)
+            .then(
+                res => {
+                    localStorage.setItem('token', res.data.token);
 
-        }
-    )
-}
+                }
+            )
+    }
 
-    return(
-            <LoginScreen>
-                <Nav>
+    return (
+        <LoginScreen>
+            <Nav>
                 <H3>Studium</H3>
-                </Nav>
-                <GoogButton>
-            <GoogleButton
-                type="dark"
-                onClick={() => { console.log('This button can be clicked') }}
-            /></GoogButton>
+            </Nav>
+            <GoogButton>
+                <GoogleButton
+                    type="dark"
+                    onClick={() => { console.log('This button can be clicked') }}
+                /></GoogButton>
             <HRline></HRline>
             <H4>Or Email</H4>
             <HRline2></HRline2>
-              <form
-            onSubmit={handleSubmit}>
+            <form
+                onSubmit={handleSubmit}>
                 <TextBox
-              type="text"
-              name="username"
-              placeholder= 'Type your username'
-              value={login.username}
-              onChange={handleChange}
-              />
-              <TextBox2
-              type="password"
-              name="password"
-              placeholder="Type your password"
-              value={login.password}
-              onChange={handleChange}
-              />
-            <Button type="submit">Log In</Button>
+                    type="text"
+                    name="username"
+                    placeholder='Type your username'
+                    value={login.username}
+                    onChange={handleChange}
+                />
+                <TextBox2
+                    type="password"
+                    name="password"
+                    placeholder="Type your password"
+                    value={login.password}
+                    onChange={handleChange}
+                />
+                <Button type="submit">Log In</Button>
             </form>
-            </LoginScreen>
+        </LoginScreen>
     )
 }
 
