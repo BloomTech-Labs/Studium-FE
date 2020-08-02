eed6023ecdf65570377ebce093c246a02149376e
juniordugue <jrdugue@gmail.com>
2020-08-01 23:44:36 -0400

    Installed cypress as a devdepencies, added simple tests with cypress for visiting our app url and localhost app under form_test.js file

---
+++ cypress/integration/form_test.js
@@ -0,0 +1,16 @@
+describe("this is our first test!", () => {
+  it("should return true", () => {
+    expect(true).to.equal(true);
+  });
+});
+
+describe("testing our form inputs", () => {
+  beforeEach(function () {
+    // runs before each test in this block
+    // cy.visit("https://localhost:3000");
+    // cy.visit("https://www.studium-app.net/");
+  });
+
+  it("visits the URL", () => {});
+});
+
