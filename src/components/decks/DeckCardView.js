76f7125be5e03f758575d32a75a964d0cb28b234
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-08-02 20:19:19 -0400

    DeckCardView update

--- src/components/decks/DeckCardView.js
+++ src/components/decks/DeckCardView.js
@@ -1,17 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
+import Flippy, { FrontSide, BackSide } from 'react-flippy'
 import { CardItself, CardFront } from './styles-decks/DeckViewStyles.js'
-import { useHistory } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { Link, useHistory } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { setCardBeingEdited } from '../../redux/actions'
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
 
+const cardStyle = {
+    background: '#FFFFFF',
+    border: '1px solid #C4C4C4',
+    borderRadius: '20px',
+    width: '150px',
+    height: '192px',
+    display: 'flex',
+    flexDirection: 'column',
+    marginBottom: '2vh'
+ }
 
-
-const DeckCardView = ({ card }) => {
-
+const DeckCardView = ({ displayedCard }) => {
+    const dispatch = useDispatch()
     const userDecks = useSelector(state => state.userDecks)
-    const [deckId, setDeckId] = useState();
-
-    const { card_front } = card
     const history = useHistory()
+
+    const [deckId, setDeckId] = useState();
+    const { card_front } = displayedCard
 
     useEffect(() => {
         const getDeckId = () => {
@@ -22,12 +34,51 @@ const DeckCardView = ({ card }) => {
         getDeckId();
     }, [])
 
+    const handleClick = () => {
+        console.log('cardToEdit from the Flipper: ', displayedCard)
+        dispatch(setCardBeingEdited(displayedCard))
+        history.push(`/deck/${deckId}/card/${displayedCard.id}`)
+     }
+
     return (
-        <CardItself onClick={() => {
-            history.push(`/deck/${deckId}/card/${card.id}`)
-        }}>
-            <CardFront>{card_front}</CardFront>
-        </CardItself>
+        // <CardItself onClick={() => {
+        //     history.push(`/deck/${deckId}/card/${card.id}`)
+        // }}>
+        //     <CardFront>{card_front}</CardFront>
+        // </CardItself>
+
+        <Flippy
+            flipOnClick={true}
+            flipDirection="horizontal"
+         >
+            <FrontSide style={cardStyle} className='flippy-front'>
+                <Link 
+                    to={`/deck/${displayedCard.deck_id}/edit-card/${displayedCard.id}`}
+                    style={{ textDecoration: 'none', color: 'grey', textAlign: 'right', paddingRight: '10px', marginTop: '10px', marginBottom: '6px' }}
+                >
+                    <div onClick={handleClick}>
+                        <EditOutlinedIcon color='inherit'/>
+                    </div>
+                </Link>
+                <div>
+                    {displayedCard.card_front}
+                </div>
+            </FrontSide>
+            
+            <BackSide style={cardStyle} className='flippy-back'>
+                <Link 
+                    to={`/deck/${displayedCard.deck_id}/edit-card/${displayedCard.id}`}
+                    style={{ textDecoration: 'none', color: 'grey', textAlign: 'right', paddingRight: '10px', marginTop: '10px', marginBottom: '6px' }}
+                >
+                    <div onClick={handleClick}>
+                      <EditOutlinedIcon />
+                    </div>
+                </Link>
+                <div>
+                    {displayedCard.card_back}
+                </div>
+            </BackSide>
+         </Flippy>
     )
 }
 
