d2b698cc8a8a6e0210f0ffc13f58d4b0a20a9ea9
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-08-16 23:29:11 -0400

    hitting the metrics endpoint

--- src/components/decks/DeckView.js
+++ src/components/decks/DeckView.js
@@ -3,13 +3,15 @@ import { useSelector, useDispatch } from 'react-redux'
 import NavbarDash from '../navigation/NavBarDash.js'
 import DeckViewCards from './DeckViewCards.js'
 import { useParams, Link } from 'react-router-dom'
-import { setDeckBeingEdited, setCurrentSession } from '../../redux/actions'
+import { setDeckBeingEdited, setCurrentSession, getDeckSessions } from '../../redux/actions'
+import AxiosWithAuth from '../../utils/axiosWithAuth'
 
 import { MainWrapper, H1, H2, NamesWrapper, FooterWrapper, EditButton, StudyButton } from './styles-decks/DeckViewStyles.js'
 
 const DeckView = ({ match, location }) => {
    const dispatch = useDispatch()
    const userDecks = useSelector(state => state.userDecks)
+   const deckSessions = useSelector(state => state.deckSessions)
    const [deckName, setDeckName] = useState()
    const [deckToEdit, setDeckToEdit] = useState()
    const [sessionToPost, setSessionToPost] = useState({
@@ -34,6 +36,7 @@ const DeckView = ({ match, location }) => {
             })
          }
       })
+      dispatch(getDeckSessions(id))
    }, [])
 
    const handleEditClick = () => {
@@ -54,6 +57,22 @@ const DeckView = ({ match, location }) => {
       dispatch(setCurrentSession(sessionToPost))
    }
 
+   const getMetrics = () => {
+      const sessionData = deckSessions.map(session => {
+         return {
+            id: session.id,
+            total_looked_at: session.total_looked_at,
+            session_start: parseInt(session.session_start),
+            session_end: parseInt(session.session_end)
+         }
+      })
+      console.log('sessionData-->', sessionData)
+      AxiosWithAuth()
+         .get('https://studium-ds.herokuapp.com/metrics', sessionData)
+         .then(res => console.log('metricsResponse-->', res))
+         .catch(err => console.log('metricsError-->', err))
+   }
+
    return (
       <MainWrapper>
          <NavbarDash />
@@ -66,6 +85,7 @@ const DeckView = ({ match, location }) => {
             <Link to={`/deck/${id}/edit-deck`}>
                <EditButton onClick={handleEditClick}>Edit Deck</EditButton>
             </Link>
+            <button onClick={getMetrics}>Try Metrics GET</button>
             <Link to={`/deck/${id}/study`}>
                <StudyButton 
                   onMouseDown={handleStudyMousedown}
