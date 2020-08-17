3f9fcd5b967e9ad1f87f582929423cf5c724d8cc
Anthony Lopez <anthonybeconscious@gmail.com>
2020-08-16 21:33:39 -0500

    adjusted _reduxMock_. Got the Dashboard.test to render

--- src/components/dashboard/Dashboard.test.js
+++ src/components/dashboard/Dashboard.test.js
@@ -1,10 +1,13 @@
-// import React from "react";
-// import Dashboard from './DashBoard'
-// import { renderWithRedux } from "../../../__mocks__/reduxMock";
+import React from "react";
+import Dashboard from './DashBoard'
+import { renderWithRedux } from "../../../__mocks__/reduxMock";
+import { BrowserRouter as Router } from "react-router-dom";
 
 
-// test("renders without crashing", async () => {
-//     renderWithRedux(
-//         <Dashboard/>
-//     )
-// });
+test("renders without crashing", async () => {
+    renderWithRedux(
+        <Router>
+            <Dashboard/>
+        </Router>
+    )
+});
